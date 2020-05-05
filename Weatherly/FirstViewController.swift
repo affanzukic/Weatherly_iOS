909b0daf5d27956dfc9b30b9b29efbdb97de7e5e
Affan Zukic <affan.zukic@icloud.com>
2020-05-06 01:50:58 +0200

    Initial commit

---
+++ Weatherly/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Weatherly
+//
+//  Created by Affan Zukić on 2020-05-06.
+//  Copyright © 2020 Affan Zukić. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
